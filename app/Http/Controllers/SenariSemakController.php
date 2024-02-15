174542577f2394bf8424c7397122056837cfdeca
umar joriya <umer@gmail.com>
2024-02-15 15:16:30 +0500

    push

--- app/Http/Controllers/SenariSemakController.php
+++ app/Http/Controllers/SenariSemakController.php
@@ -288,7 +288,7 @@ class SenariSemakController extends Controller
         $page = $request->input('page', 1);
         $search = $request->input('q');
 
-        $query = SaleOrder::select('id', 'order_no');
+        $query = SaleOrder::select('id', 'order_no')->where('order_status', '=', 'published');
         if ($search) {
             $query->where('order_no', 'like', '%' . $search . '%');
         }
