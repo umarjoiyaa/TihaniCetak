367ed284e11cebc5b9f14d774a90fefbfb111dc9
Hassan Absar <hassanabsar80@gmail.com>
2024-02-18 17:55:31 +0500

    Datatables changes added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,8 @@
     <!--- Animations css-->
     <link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet">
 
+    <!-- Internal Datatable css-->
+    <link href="{{ asset('assets/plugins/datatable/css/dataTables.dataTables.css') }}" rel="stylesheet">
 
     <!-- Internal Chart-Morris css-->
     <link href="{{ asset('assets/plugins/morris.js/morris.css') }}" rel="stylesheet">
@@ -2054,22 +2056,9 @@
 
 
     <!-- Internal Data tables -->
-    <script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/dataTables.dataTables.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/responsive.dataTables.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/dataTables.bootstrap4.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/jszip.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/pdfmake.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/vfs_fonts.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/buttons.print.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/buttons.colVis.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('/assets/plugins/datatable/js/dataTables.js') }}"></script>
+
+
 
     <!-- Internal Datatable js -->
     {{-- <script src="{{ asset('assets/js/table-data.js') }}"></script> --}}
