7d2da5e81bd9c6532019ed630f5372c7dc045cf4
Wasay Gujjer <wasaygujjer@gmail.com>
2024-02-23 19:45:07 +0500

    commit

--- resources/views/Setting/Department/create.blade.php
+++ resources/views/Setting/Department/create.blade.php
@@ -16,7 +16,7 @@
                             <div class="col-md-4">
                                 <div class="control-group form-group">
                                     <label class="form-label">Department Name</label>
-                                    <input type="text" class="form-control required" name="name" value="{{old('name')}}">
+                                    <input type="text" class="form-control" name="name" value="{{old('name')}}">
                                 </div>
                             </div>
                     </div>
