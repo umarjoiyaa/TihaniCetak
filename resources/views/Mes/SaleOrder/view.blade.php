083c4d464b7f7ea913f3bef57d537dc3eb0d960f
Wasay Gujjer <wasaygujjer@gmail.com>
2024-03-05 21:40:36 +0500

    3/5/2024 9:40pm

--- resources/views/Mes/SaleOrder/view.blade.php
+++ resources/views/Mes/SaleOrder/view.blade.php
@@ -24,7 +24,7 @@
                             <h4><b>SALES ORDER</b></h4>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-6">
                         <div class="box" style="width:400px; padding:10px; height:180px; border:1px solid black;">
                             <p>EDUKID DISTRIBUTORS SDN BHD</p>
                             <p>
@@ -38,55 +38,50 @@
 
                         </div>
                     </div>
-                    <div class="col-md-8">
-                        <div class="text-center" style="margin-left:200px; width:600px;">
-                            <div class="row mt-2">
-                                <div class="col-sm-5 col-md-2">
-                                    <p>Your Ref No. </p>
-                                </div>
-                                <div class="col-sm-4 offset-sm-2 col-md-3 offset-md-0"><span>:</span>
-                                <snap >{{ $sale_order->order_no }}</span>
-
-                                </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>Your Ref No. </p>
                             </div>
+                            <div class="col-md-4">
+                            <snap ><span>: </span>{{ $sale_order->order_no }}</span>
 
-                            <div class="row mt-2">
-                                <div class="col-sm-5 col-md-2">
-                                    <p>Our Ref No</p>
-                                </div>
-                                <div class="col-sm-4 offset-sm-2 col-md-3 offset-md-0"><span style="margin-left:-40px;">:</span>
-                                </div>
                             </div>
-
-                            <!-- <div class="row">
-                                <div class="col-sm-5 col-md-6">
-                                    <p>Term</p>
-                                </div>
-                                <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0"> <span
-                                        >:</span>
-                                    <snap >{{ $sale_order->po_no }}</span>
-                                </div>
-                            </div> -->
-
-                            <div class="row mt-2">
-                                <div class="col-sm-5 col-md-2">
-                                    <p>Terms </p>
-                                </div>
-                                <div class="col-sm-5 offset-sm-2 col-md-3 offset-md-0"><span
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>Our Ref No</p>
+                            </div>
+                            <div class="col-md-4">
+                                <span style="margin-left:-40px;">:</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>Terms </p>
+                            </div>
+                            <div class="col-md-4">
+                                <span
                                 style="margin-left:-10px;" >:</span>
                                     <snap>{{ $sale_order->terms }}</span>
-                                </div>
                             </div>
+                        </div>
 
-                            <div class="row mt-2">
-                                <div class="col-sm-5 col-md-2">Date</div>
-                                <div class="col-sm-3 offset-sm-2 col-md-3 offset-md-0"><span style="margin-left:40px;">:</span>
-                                    <snap>{{ $sale_order->date }}</span></div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>Date</p>
                             </div>
-                            <div class="row mt-2">
-                                <div class="col-sm-5 col-md-2">Page</div>
-                                <div class="col-sm-5 offset-sm-2 col-md-3 offset-md-0"><span style="margin-left:-30px;">:</span>
-                                    <!-- <snap>{{ $sale_order->date }}</span></div>  -->
+                            <div class="col-md-4">
+                            <span style="margin-left:40px;">:</span>
+                                    <snap>{{ $sale_order->date }}</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>Page</p>
+                            </div>
+                            <div class="col-md-4">
+                            <span style="margin-left:-30px;">:</span>
                             </div>
                         </div>
                     </div>
@@ -122,85 +117,90 @@
                 </div>
 
                 
-                <div class="row w-100">
+                <div class="row w-100 d-flex justify-content-center">
 
                     <div class="col-md-2"></div>
-                    <div class="col-md-8">
-                        <table class="table table-bordered">
-                            <tbody>
-                                <tr>
-                                    <td>Status</td>
-                                    <td colspan="2"><b>{{ $sale_order->status }}</b></td>
-                                </tr>
-                                <tr>
-                                    <td>Kod Buku</td>
-                                    <td colspan="2">{{ $sale_order->kod_buku }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Catekan</td>
-                                    <td colspan="2">{{ $sale_order->catekan }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Size</td>
-                                    <td colspan="2">{{ $sale_order->size }}</td>
-                                </tr>
-                                <tr>
-                                    <td rowspan="2">Pages</td>
-                                    <td>Cover</td>
-                                    <td>{{ $sale_order->pages_cover }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Text </td>
-                                    <td>{{ $sale_order->pages_text }}</td>
-                                </tr>
-                                <tr>
-                                    <td rowspan="2">Paper</td>
-                                    <td>cover</td>
-                                    <td>{{ $sale_order->paper_cover }}</td>
-                                </tr>
-                                <tr>
-                                    <td>text </td>
-                                    <td>{{ $sale_order->paper_text }}</td>
-                                </tr>
+                    <div class="col-md-6 ">
+                        <div class="table-responsive ">
+                            <table class="table table-bordered ">
+                                <tbody>
+                                    <tr>
+                                        <td>Status</td>
+                                        <td colspan="2"><b>{{ $sale_order->status }}</b></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Kod Buku</td>
+                                        <td colspan="2">{{ $sale_order->kod_buku }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Cetakan</td>
+                                        <td colspan="2">{{ $sale_order->catekan }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Size</td>
+                                        <td colspan="2">{{ $sale_order->size }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td rowspan="2">Pages</td>
+                                        <td>Cover</td>
+                                        <td>{{ $sale_order->pages_cover }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Text </td>
+                                        <td>{{ $sale_order->pages_text }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td rowspan="2">Paper</td>
+                                        <td>cover</td>
+                                        <td>{{ $sale_order->paper_cover }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>text </td>
+                                        <td>{{ $sale_order->paper_text }}</td>
+                                    </tr>
 
-                                <tr>
-                                    <td rowspan="2">Priting</td>
-                                    <td>Cover</td>
-                                    <td>{{ $sale_order->printing_cover }}</td>
-                                </tr>
-                                <tr>
-                                    <td>text</td>
-                                    <td>{{ $sale_order->printing_text }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Finishing</td>
-                                    <td colspan="2">{{ $sale_order->finishing }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Binding</td>
-                                    <td colspan="2">{{ $sale_order->binding }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Shrinking wrapping</td>
-                                    <td colspan="2">{{ $sale_order->shrinking_wrapping }}</td>
-                                </tr>
+                                    <tr>
+                                        <td rowspan="2">Priting</td>
+                                        <td>Cover</td>
+                                        <td>{{ $sale_order->printing_cover }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>text</td>
+                                        <td>{{ $sale_order->printing_text }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Finishing</td>
+                                        <td colspan="2">{{ $sale_order->finishing }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Binding</td>
+                                        <td colspan="2">{{ $sale_order->binding }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Shrinking wrapping</td>
+                                        <td colspan="2">{{ $sale_order->shrinking_wrapping }}</td>
+                                    </tr>
 
-                                <tr>
-                                    <td>Extra Stock</td>
-                                    <td colspan="2">{{ $sale_order->extra_stock }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Remarks</td>
-                                    <td colspan="2">{{ $sale_order->remarks }}</td>
-                                </tr>
+                                    <tr>
+                                        <td><b>Extra Stock</b></td>
+                                        <td colspan="2">{{ $sale_order->extra_stock }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Remarks</td>
+                                        <td colspan="2">{{ $sale_order->remarks }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Sample</td>
+                                        <td colspan="2"></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Delivery Date</td>
+                                        <td colspan="2">{{ $sale_order->delivery_date }}</td>
+                                    </tr>
 
-                                <tr>
-                                    <td>Delivery Date</td>
-                                    <td colspan="2">{{ $sale_order->delivery_date }}</td>
-                                </tr>
-
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                     <div class="col-md-2"></div>
                 </div>
@@ -219,57 +219,63 @@
                     </div>
                 </div>
 
-                <div class="row mt-5">
+                <div class="row w-100">
                     <div class="col-md-12">
-                        <h3><b>Apprved By</b></h3>
-                    </div>
-                    <div class="col-md-12">
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>Username</th>
-                                    <th>Desgination</th>
-                                    <th>Department</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{{ $sale_order->approved_by_date }}</td>
-                                    <td>{{ $sale_order->approved_by_user }}</td>
-                                    <td>{{ $sale_order->approved_by_designation }}</td>
-                                    <td>{{ $sale_order->approved_by_department }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div class="row mt-3">
+                            <div class="col-md-12">
+                                <h3><b>Apprved By</b></h3>
+                            </div>
+                            <div class="col-md-12">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>Date</th>
+                                            <th>Username</th>
+                                            <th>Desgination</th>
+                                            <th>Department</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>{{ $sale_order->approved_by_date }}</td>
+                                            <td>{{ $sale_order->approved_by_user }}</td>
+                                            <td>{{ $sale_order->approved_by_designation }}</td>
+                                            <td>{{ $sale_order->approved_by_department }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-12">
+                                <h3><b>Published By</b></h3>
+                            </div>
+                            <div class="col-md-12">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>Date</th>
+                                            <th>Username</th>
+                                            <th>Desgination</th>
+                                            <th>Department</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>{{ $sale_order->published_by_date }}</td>
+                                            <td>{{ $sale_order->published_by_user }}</td>
+                                            <td>{{ $sale_order->published_by_designation }}</td>
+                                            <td>{{ $sale_order->published_by_department }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                
 
-                <div class="row mt-5">
-                    <div class="col-md-12">
-                        <h3><b>Published By</b></h3>
-                    </div>
-                    <div class="col-md-12">
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>Username</th>
-                                    <th>Desgination</th>
-                                    <th>Department</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{{ $sale_order->published_by_date }}</td>
-                                    <td>{{ $sale_order->published_by_user }}</td>
-                                    <td>{{ $sale_order->published_by_designation }}</td>
-                                    <td>{{ $sale_order->published_by_department }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                
 
             </div>
         </div>
